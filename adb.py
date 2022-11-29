46c5f4b1a4b726f9756d65c389e22659f1d1811e
LeslieFu <lesliefu@yeah.net>
2022-11-30 00:29:40 +0800

    完善了一下删除的逻辑，只删除文件不删除目录

--- adb.py
+++ adb.py
@@ -2,14 +2,25 @@ import os
 import subprocess
 # adb工具路径
 adb_path = "D:\CSTCloud\\freshman\SoftwareEngineering\Program\platform-tools\\adb.exe"
-# 数据集路径
+
+def add_sdcard_files(dataset_path, sdcard_path):
+    """将电脑上的某文件夹下的所有文件传输至手机上的可读写特定路径"""
+    for root, dirs, files in os.walk(dataset_path):
+        for file in files:
+            file_local = dataset_path +"\\" + file
+            print(file_local)
+            file_remote = sdcard_path + "/" + file
+            subprocess.call([adb_path, "push", file_local, file_remote])
+
+def delete_sdcard_files(sdcard_path):
+    """删除sdcard_path目录下的所有文件，但不删除目录本身"""
+    if sdcard_path[-1] == '/':
+        sdcard_path = sdcard_path + "*"
+    else:
+        sdcard_path = sdcard_path + "/*"
+    subprocess.call([adb_path, "shell", "rm", "-rf", sdcard_path])
+# test
 dataset_path = "D:\CSTCloud\\freshman\SoftwareEngineering\Program\dataset"
-# 目的设备上的数据集路径
 sdcard_path = "/sdcard/Simplify/tmpDataset"
-for root, dirs, files in os.walk(dataset_path):
-    for file in files:
-        file_local = dataset_path +"\\" + file
-        print(file_local)
-        file_remote = sdcard_path + "/" + file
-        subprocess.call([adb_path, "push", file_local, file_remote])
-subprocess.call([adb_path, "shell", "rm", sdcard_path])
+add_sdcard_files(dataset_path, sdcard_path)
+delete_sdcard_files(sdcard_path)
